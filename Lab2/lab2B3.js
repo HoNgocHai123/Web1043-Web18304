f017e4310b9dfd96b983aebe24bf52fcf6249f66
Hai Ho <haihnpd08010@fpt.edu.vn>
2023-03-20 17:03:19 +0700

    noplab2

--- Lab2/lab2B3.js
+++ Lab2/lab2B3.js
@@ -1,18 +1,18 @@
- let fullName = prompt("Nhập vào họ và tên của bạn:"); // nhập tên
-      let gender = prompt("Nhập vào giới tính của bạn (Nam hoặc Nữ):"); // nhập giới tính
+ let fullName = prompt("Nhập vào họ và tên của bạn:");
+      let gender = prompt("Nhập vào giới tính của bạn (Nam hoặc Nữ):"); 
 
-      let nameParts = fullName.split(" "); // tách cả họ tên thành những phần riêng dựa trên khoảng trắng
-      let firstName = nameParts.pop(); // lấy phần tử cuối cùng  của chuỗi đã tách ở trên
-      let lastName = nameParts.shift(); // lấy phần tử đầu tiên ...
-      let middleName = nameParts.join(" "); // ghép các phần tử không được lấy thành tên đệm
+      let nameParts = fullName.split(" "); 
+      let firstName = nameParts.pop(); 
+      let lastName = nameParts.shift(); 
+      let middleName = nameParts.join(" "); 
 
       if (gender.toLowerCase() === "nam") {
-        middleName = "Văn"; // kiểm tra xem giới tính nhập vào có phải là nam không nếu phải thì đổi tên đệm thành văn
+        middleName = "Văn"; 
       } else if (gender.toLowerCase() === "nữ") {
-        middleName = "Vẽ"; // kiểm tra xem nếu là nữ thì ... đổi họ đệm thành nữ
+        middleName = "Vẽ"; 
       }
 
-      let newFullName = lastName + " " + middleName + " " + firstName; // cho ra một tên hoàn chỉnh sau khi chỉnh sửa
+      let newFullName = lastName + " " + middleName + " " + firstName; 
 
       //in ra kết quả tên mới... in hoa cho nó rực rỡ
       console.log("Tên ban đầu : " + fullName.toUpperCase());
